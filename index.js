1a5791689d00518b6596e85d0a558062601bc63c
BelanVaidehee <133239808+BelanVaidehee@users.noreply.github.com>
2025-05-26 23:21:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,12 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'testdb'
-});
-
-app.get('/user', (req, res) => {
-  const userId = req.query.id;
-  const query = `SELECT * FROM users WHERE id = ${userId}`;
-  
-  connection.query(query, (err, results) => {
-    if (err) throw err;
-    res.send(results);
-  });
-});
+});*/
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
